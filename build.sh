fcef52f8929ae8c26323c2569f5058c2fe8c3a1e
intrigus <abc123zeus@live.de>
2016-10-11 18:43:16 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #/bin/sh
 set -e
 
-VERSION=1.0.1
+VERSION=2.0.3
 mvn -q dependency:get -Dartifact="co.leantechniques:maven-buildtime-extension:$VERSION"
 
 mvn clean install -Dmaven.ext.class.path="$HOME/.m2/repository/co/leantechniques/maven-buildtime-extension/$VERSION/maven-buildtime-extension-$VERSION.jar"
