a754d8cf2afd1c95c8ef0d0743d97740f8036f82
intrigus <abc123zeus@live.de>
2016-10-11 17:19:27 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #/bin/sh
 set -e
-mvn clean install -DskipTests
+mvn clean install
 mvn -f plugins/idea/pom.xml clean install -Didea.home=$HOME/idea-15
 mvn -f plugins/eclipse/pom.xml clean install
 ./plugins/gradle/gradlew -b plugins/gradle/build.gradle clean assemble install
