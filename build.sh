dd1bae0a95b3b74d11a8585f716fd2cace2f3648
intrigus <abc123zeus@live.de>
2016-10-11 20:28:09 +0200

    Update files

--- build.sh
+++ build.sh
@@ -1,10 +1,7 @@
 #/bin/sh
 set -e
 
-VERSION=2.0.3
-mvn -q dependency:get -Dartifact="co.leantechniques:maven-buildtime-extension:$VERSION"
-
-mvn clean install -Dmaven.ext.class.path="$HOME/.m2/repository/co/leantechniques/maven-buildtime-extension/$VERSION/maven-buildtime-extension-$VERSION.jar"
-mvn -f plugins/idea/pom.xml clean install -Didea.home=$HOME/idea-15 -Dmaven.ext.class.path="$HOME/.m2/repository/co/leantechniques/maven-buildtime-extension/$VERSION/maven-buildtime-extension-$VERSION.jar"
-mvn -f plugins/eclipse/pom.xml clean install -Dmaven.ext.class.path="$HOME/.m2/repository/co/leantechniques/maven-buildtime-extension/$VERSION/maven-buildtime-extension-$VERSION.jar"
+mvn clean install -DskipTests
+mvn -f plugins/idea/pom.xml clean install -Didea.home=$HOME/idea-15
+mvn -f plugins/eclipse/pom.xml clean install
 ./plugins/gradle/gradlew -b plugins/gradle/build.gradle clean assemble install
