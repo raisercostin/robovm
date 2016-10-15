2f305fa67c6e8fa27778b66e234f1f89c929c119
intrigus <abc123zeus@live.de>
2016-10-15 21:20:14 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #/bin/sh
 set -e
-cd compiler/libimobiledevice
-./build.sh
-cd ../..
+#cd compiler/libimobiledevice
+#./build.sh
+#cd ../..
 mvn clean install
 mvn -f plugins/idea/pom.xml clean install -Didea.home=$HOME/idea-15
 mvn -f plugins/eclipse/pom.xml clean install
